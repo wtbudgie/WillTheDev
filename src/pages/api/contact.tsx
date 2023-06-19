62d14332cd672cd7c8b1a8540223a46f1db40b96
wtbudgie <maniakwill@gmail.com>
2023-06-19 19:55:22 +1000

    add rate limits to contact button

--- src/pages/api/contact.tsx
+++ src/pages/api/contact.tsx
@@ -1,26 +1,55 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
+import rateLimit, { ValueDeterminingMiddleware } from "express-rate-limit";
+
+const keyGenerator: ValueDeterminingMiddleware<string> = (
+  req: NextApiRequest
+) => {
+  return (
+    (req.headers["x-forwarded-for"] as string) ||
+    req.connection.remoteAddress ||
+    ""
+  );
+};
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 1,
+  keyGenerator: keyGenerator,
+});
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const email = req.body.email;
-  const name = req.body.name;
-  const subject = req.body.subject;
-  const content = req.body.content;
+  limiter(req, res, (err: any) => {
+    if (err) {
+      return res.status(429).json({
+        success: false,
+        message: "Too many requests, please try again later.",
+        status: 429,
+      });
+    }
 
-  const webhookUrl =
-    "https://discord.com/api/webhooks/1118315502125715579/WTfglNaOHHZDfF1veWWRnuij8HVVBJC8k-QS-7QSJroAZpQgewgZuh3gOAwPNYLwH5In";
-  const message = {
-    content: `Email: **${email}** - Name: **${name}**\nSubject: \`${subject}\`\n\`------------------------\`\n\n${content}`,
-  };
+    const email = req.body.email;
+    const name = req.body.name;
+    const subject = req.body.subject;
+    const content = req.body.content;
 
-  axios
-    .post(webhookUrl, message)
-    .then((response) => {
-      console.log("Message sent successfully:", response.data);
-    })
-    .catch((error) => {
-      console.error("Error sending message to Discord:", error);
-    });
+    const webhookUrl =
+      "https://discord.com/api/webhooks/1118315502125715579/WTfglNaOHHZDfF1veWWRnuij8HVVBJC8k-QS-7QSJroAZpQgewgZuh3gOAwPNYLwH5In";
+    const message = {
+      content: `Email: **${email}** - Name: **${name}**\nSubject: \`${subject}\`\n\`------------------------\`\n\n${content}`,
+    };
 
-  res.status(200).json({ success: true, message: "Sent message." });
+    axios
+      .post(webhookUrl, message)
+      .then((response) => {
+        console.log("Message sent successfully:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error sending message to Discord:", error);
+      });
+
+    res
+      .status(200)
+      .json({ success: true, message: "Sent message.", status: 200 });
+  });
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,14 @@ import axios from "axios";
 const Home: NextPage = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [showNotification, setShowNotification] = useState(false);
+  const [showErrorNotification, setShowErrorNotification] = useState(false);
 
   useEffect(() => {
     let timer: NodeJS.Timeout;
-    if (showNotification) {
+    if (showNotification || showErrorNotification) {
       timer = setTimeout(() => {
         setShowNotification(false);
+        setShowErrorNotification(false);
       }, 6000);
     }
     return () => clearTimeout(timer);
@@ -127,6 +129,28 @@ const Home: NextPage = () => {
                 </svg>
               </span>
               Successfully sent the contact request!
+            </div>
+          )}
+          {showErrorNotification && (
+            <div
+              className="fixed bottom-4 right-4 mb-3 inline-flex items-center rounded-lg bg-red-500 px-6 py-5 text-base text-white"
+              role="alert"
+            >
+              <span className="mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              Too many contact requests have been sent. Try again later.
             </div>
           )}
 
@@ -260,11 +284,17 @@ const Home: NextPage = () => {
                         content,
                       })
                       .then((response) => {
-                        setIsOpen(false);
-                        setShowNotification(true);
+                        if (response.status == 429) {
+                          setIsOpen(false);
+                          setShowErrorNotification(true);
+                        } else {
+                          setIsOpen(false);
+                          setShowNotification(true);
+                        }
                       })
                       .catch((error) => {
-                        console.error("Error sending contact request:", error);
+                        setIsOpen(false);
+                        setShowErrorNotification(true);
                       });
                   }}
                 >
