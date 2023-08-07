cc9378fbc76694400c16f2e025f0403eee10e90b
willthedev <48879094+wtbudgie@users.noreply.github.com>
2023-08-07 20:30:28 +1000

    fix email

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,7 @@ const Home: NextPage = () => {
             <a
               className="inline-flex items-center rounded border-b-4 border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white transition hover:border-blue-500 hover:bg-blue-400"
               onClick={() => {
-                navigator.clipboard.writeText("willthedev.com");
+                navigator.clipboard.writeText("will@willthedev.com");
               }}
             >
               <FaEnvelope />  will@willthedev.com
