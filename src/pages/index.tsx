e53d1786d5fd6141f16fa0cabe2aa2ef9417ef59
willthedev <48879094+wtbudgie@users.noreply.github.com>
2023-08-07 20:24:58 +1000

    remove contact form and replace with email copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,12 @@
 import { NextPage } from "next";
-import { FaDiscord, FaGithub, FaMailBulk } from "react-icons/fa";
+import { FaGithub, FaEnvelope } from "react-icons/fa";
 import Head from "next/head";
 import { TailwindCss, Nodejs } from "@styled-icons/boxicons-logos";
 import { Nextdotjs, Typescript, Mongodb } from "@styled-icons/simple-icons";
-import { ChangeEvent, useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
+import React, { useState } from "react";
 
 const Home: NextPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [showNotification, setShowNotification] = useState(false);
-  const [showRTErrorNotification, setShowRTErrorNotification] = useState(false);
-  const [showBRErrorNotification, setShowBRErrorNotification] = useState(false);
-
-  const maxLength = 1500;
-  const [text, setText] = useState("");
-
-  useEffect(() => {
-    let timer: NodeJS.Timeout;
-    if (
-      showNotification ||
-      showRTErrorNotification ||
-      showBRErrorNotification
-    ) {
-      timer = setTimeout(() => {
-        setShowNotification(false);
-        setShowRTErrorNotification(false);
-        setShowBRErrorNotification(false);
-      }, 6000);
-    }
-    return () => clearTimeout(timer);
-  }, [showNotification]);
+  const [emailPanelOpen, setEmailPanelOpen] = useState(false);
 
   return (
     <>
@@ -69,27 +46,13 @@ const Home: NextPage = () => {
               <FaGithub />
             </a>
             <a
-              href="https://willthedev.com/discord"
               className="inline-flex items-center rounded border-b-4 border-blue-700 bg-blue-500 px-4 py-2 font-bold text-white transition hover:border-blue-500 hover:bg-blue-400"
-            >
-              Discord 
-              <FaDiscord fontSize={"20px"} />
-            </a>
-            <button
-              id="contact-button"
-              className={`inline-flex items-center rounded border-b-4 ${
-                isOpen
-                  ? "border-green-500 bg-green-400"
-                  : "border-blue-700 bg-blue-500"
-              } px-4 py-2 font-bold text-white transition hover:border-blue-500 hover:bg-blue-400`}
-              type="button"
               onClick={() => {
-                setIsOpen(true);
+                navigator.clipboard.writeText("willthedev.com");
               }}
             >
-              Contact &nbsp;
-              <FaMailBulk fontSize="20px" />
-            </button>
+              <FaEnvelope />  will@willthedev.com
+            </a>
           </div>
 
           <div className="mt-24">
@@ -118,72 +81,6 @@ const Home: NextPage = () => {
               />
             </div>
           </div>
-          {showNotification && (
-            <div
-              className="fixed bottom-4 right-4 mb-3 inline-flex items-center rounded-lg bg-green-100 px-6 py-5 text-base text-green-700"
-              role="alert"
-            >
-              <span className="mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-5 w-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </span>
-              Successfully sent the contact request!
-            </div>
-          )}
-          {showRTErrorNotification && (
-            <div
-              className="fixed bottom-4 right-4 mb-3 inline-flex items-center rounded-lg bg-red-500 px-6 py-5 text-base text-white"
-              role="alert"
-            >
-              <span className="mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-5 w-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
-              Too many contact requests have been sent. Try again later.
-            </div>
-          )}
-          {showBRErrorNotification && (
-            <div
-              className="fixed bottom-4 right-4 mb-3 inline-flex items-center rounded-lg bg-red-500 px-6 py-5 text-base text-white"
-              role="alert"
-            >
-              <span className="mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-5 w-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
-              An error has occured, try again later.
-            </div>
-          )}
 
           <div className="mb-5 mt-28">
             <h2 className="text-5xl font-bold tracking-tight text-white">
@@ -254,182 +151,7 @@ const Home: NextPage = () => {
                 </div>
               </div>
             </div>
-          </div>
-          <br />
-          <br />
-          <br />
-          <br />
-        </div>
-        <div
-          id="authentication-modal"
-          tabIndex={-1}
-          aria-hidden={!isOpen}
-          className={`fixed left-0 right-0 top-0 z-10 ${
-            isOpen ? "" : "hidden"
-          } md:inset-0place-content-center flex h-[calc(100%-1rem)] max-h-full w-full place-content-center justify-center overflow-y-auto overflow-x-hidden p-20`}
-        >
-          <div className="relative max-h-full w-full max-w-4xl">
-            <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">
-              <button
-                type="button"
-                className="absolute right-2.5 top-3 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white"
-                data-modal-hide="authentication-modal"
-                onClick={() => {
-                  setIsOpen(false);
-                }}
-              >
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
-              <div className="px-6 py-6 lg:px-8">
-                <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-                  Send a contact message.
-                </h3>
-                <form
-                  className="space-y-6"
-                  onSubmit={(event: any) => {
-                    event.preventDefault();
-                    const formData = new FormData(event.target);
-                    const email = formData.get("email");
-                    const name = formData.get("name");
-                    const subject = formData.get("subject");
-                    const content = formData.get("content");
-
-                    axios
-                      .post("/api/contact", {
-                        email,
-                        name,
-                        subject,
-                        content,
-                      })
-                      .then((response) => {
-                        setIsOpen(false);
-                        setShowNotification(true);
-                      })
-                      .catch((error: AxiosError) => {
-                        if (error.status == 429) {
-                          setIsOpen(false);
-                          setShowRTErrorNotification(true);
-                        } else {
-                          setIsOpen(false);
-                          setShowBRErrorNotification(true);
-                        }
-                      });
-                  }}
-                >
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Your email
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                      placeholder="name@company.com"
-                      maxLength={50}
-                      required
-                    />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Your name
-                    </label>
-                    <input
-                      type="text"
-                      name="name"
-                      id="name"
-                      placeholder="Will"
-                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                      maxLength={50}
-                      required
-                    />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Subject
-                    </label>
-                    <input
-                      type="text"
-                      name="subject"
-                      id="subject"
-                      placeholder="Paid commission request."
-                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                      maxLength={75}
-                      required
-                    />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Email Content
-                    </label>
-                    <textarea
-                      name="content"
-                      id="content"
-                      placeholder="..."
-                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                      required
-                      maxLength={maxLength}
-                      onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
-                        const inputText = event.target.value;
-                        if (inputText.length <= maxLength) {
-                          setText(inputText);
-                        }
-                      }}
-                    />
-                    <div
-                      style={{
-                        marginTop: "5px",
-                        marginLeft: "auto",
-                        marginRight: 0,
-                      }}
-                    >
-                      <label
-                        style={{
-                          color: "white",
-                          marginLeft: "auto",
-                          marginRight: "15px",
-                          float: "right",
-                        }}
-                      >
-                        {text.length}/{maxLength} Characters
-                      </label>
-                    </div>
-                  </div>
-
-                  <button
-                    type="submit"
-                    className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Send the request.
-                  </button>
-                </form>
-              </div>
-            </div>
+            <div style={{ padding: "50px" }}></div>
           </div>
         </div>
       </main>
